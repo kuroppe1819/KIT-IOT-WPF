8222b77585cba5ef02c76501418725bb37e280bc
kuroppe1819 <kuroppems@gmail.com>
2018-10-12 16:08:26 +0900

    Add class for controller and model

---
+++ SerialCommunicateWpfApp/Controllers/MainController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerialCommunicateWpfApp.Controller
+{
+    class MainController
+    {
+        public void hoge() {
+            Console.WriteLine("hoge");
+        }
+    }
+}

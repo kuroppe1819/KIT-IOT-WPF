18d86fd6d21a0cf1614a073defbb63fa94e19882
kuroppe1819 <kuroppems@gmail.com>
2018-10-13 20:52:18 +0900

    Added Exception when failed to read bytes

---
+++ SerialCommunicateWpfApp/Exception/InvalidReadByteException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class InvalidReadByteException : Exception {
+    public InvalidReadByteException() : base("無効なバイトを読みこみました") { }
+}

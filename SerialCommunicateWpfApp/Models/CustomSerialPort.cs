5e90d4efdc6d98066d94019f7ec63593bbd25495
kuroppe1819 <kuroppems@gmail.com>
2018-10-13 20:53:20 +0900

    Added class named CustomSerialPort for serial communicatoin

---
+++ SerialCommunicateWpfApp/Models/CustomSerialPort.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+
+namespace SerialCommunicateWpfApp.Models {
+    class CustomSerialPort : SerialPort {
+        public CustomSerialPort() {
+            DataBits = 8;
+            Parity = Parity.None;
+            StopBits = StopBits.One;
+            Encoding = Encoding.UTF8;
+            ReadTimeout = 100000; //ミリ秒単位
+        }
+
+        public void SetDataReceiveHandler(Action<object, SerialDataReceivedEventArgs> handler) {
+            DataReceived += new SerialDataReceivedEventHandler(handler);
+        }
+
+        public void Open(string portName, int baudRate) {
+            PortName = portName;
+            BaudRate = baudRate;
+            Open();
+        }
+
+        public byte[] ReadFrames() {
+            while (ReadByte() != 0xFF) { } //スタートビットを受信するまで調整する
+            int frameLength = ReadByte();
+            var buffer = new byte[frameLength]; //フレーム長を読み取る
+            Read(buffer, 0, buffer.Length); //フレーム長とチェックサムに挟まれた分だけ読み取る
+            int checksum = ReadByte(); //チェックサムを読み取る
+            if (checksum == CustomSerialPort.CalcChecksum(buffer)) {
+                return buffer;
+            } else {
+                throw new InvalidReadByteException();
+            }
+        }
+
+        public static int CalcChecksum(byte[] buffer) {
+            int sum = 0;
+            for (int i = 0; i < buffer.Length; i++) {
+                sum += buffer[i];
+            }
+            return 0xFF - (sum & 0xFF);
+        }
+    }
+}

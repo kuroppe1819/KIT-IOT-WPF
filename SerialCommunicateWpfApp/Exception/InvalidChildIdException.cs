b9950da71e2535a2f533141a4d2c2f717cd5a2de
kuroppe1819 <kuroppems@gmail.com>
2018-10-15 14:39:56 +0900

    Throw exception that invalid child id when get it

---
+++ SerialCommunicateWpfApp/Exception/InvalidChildIdException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class InvalidChildIdException : Exception {
+    public InvalidChildIdException() : base("無効な子機IDを参照しました") { }
+}

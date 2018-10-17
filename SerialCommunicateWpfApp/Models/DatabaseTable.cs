8fc87565c3a3302a75e7943c7f15b3f4b962355f
kuroppe1819 <kuroppems@gmail.com>
2018-10-18 03:20:07 +0900

    Create the DatabaseTable.cs

---
+++ SerialCommunicateWpfApp/Models/DatabaseTable.cs
@@ -0,0 +1,21 @@
+﻿class DatabaseTable {
+    public const string CURRENT = "current";
+    public const string ENVIRONMENT = "environment";
+    public const string DUST = "dust";
+
+    public static string[] Names {
+        get { return new string[] { CURRENT, ENVIRONMENT, DUST }; }
+    }
+
+    public static class Column {
+        public const string ALL = "*";
+        public const string ID = "id";
+        public const string AREA = "area";
+        public const string DATETIME = "datetime";
+        public const string CURRENT = "current";
+        public const string TEMPERATURE = "temperature";
+        public const string HUMIDITY = "humidity";
+        public const string ILLUMINATION = "illumination";
+        public const string DUST = "dust";
+    }
+}

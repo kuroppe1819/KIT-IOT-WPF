ca54442ed8c6bc3f157a2286e7335f58311c890f
kuroppe1819 <kuroppems@gmail.com>
2018-10-24 18:18:23 +0900

    Create the command file for MySQL

---
+++ CreateKobayashiIoT.sql
@@ -0,0 +1,26 @@
+drop database if exists kobayashi_iot;
+drop user if exists sopackuser@localhost;
+create database kobayashi_iot;
+use kobayashi_iot;
+create user sopackuser@localhost identified by 'sopa-0001';
+grant all on kobayashi_iot.* to sopackuser@localhost;
+create table current (
+    id int unsigned not null primary key auto_increment,
+    area int unsigned not null,
+    datetime datetime not null,
+    current_switch boolean not null default 0
+);
+create table environment (
+    id int unsigned not null primary key auto_increment,
+    area int unsigned not null,
+    datetime datetime not null,
+    temperature int,
+    humidity int,
+    illumination int
+);
+create table dust (
+    id int unsigned not null primary key auto_increment,
+    area int unsigned not null,
+    datetime datetime not null,
+    dust int
+);
